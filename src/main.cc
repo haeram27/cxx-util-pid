85bb4b071e9d4ba7299534d61b8e022b0e38685a
haeram27 <haeram27@gmail.com>
2023-03-03 12:47:23 +0900

    add options for sleep and exitcode

--- src/main.cc
+++ src/main.cc
@@ -6,16 +6,69 @@
 #include "util/logger.h"
 
 using namespace std;
-using cmdline_args_t = vector<string>;
 
-int main(int argc, char *argv[]) {
-    cmdline_args_t args(&argv[0], &argv[argc]);
+int main(int argc, char *const *argv){
 
-    if (argc > 1) {
-        cout << "<pid>:<ppid>" << endl;
+    int sleep_time = 0;
+    int exit_code = 0;
 
-        return 0;
+    int opt;
+    while( (opt = getopt(argc, argv, "s:x:h?")) != -1) {
+        switch(opt) {
+            case 'h':
+            case '?':
+                std::cerr << R"(
+print <pid>:<ppid> of this command process
+
+Usage:
+ -s <num>
+            sleep time.
+            this program exit after sleep seconds.
+            time SHOULD be greater than 0.
+            default is 0.
+ -x <num>
+            exit code.
+            exit code can be changed by -x option.
+            default is 0.
+)" << endl;
+                exit(0);
+                break;
+            case 's':
+            {
+                string arg_sleep_time{optarg};
+                try {
+                    sleep_time = stoi(arg_sleep_time);
+                } catch (const std::exception& e){
+                    std::cerr << "Exception: " << e.what() << std::endl;
+                } catch (...) {
+                    std::cerr << "Undefined Error: " << typeid(std::current_exception()).name() << std::endl;
+                }
+
+                break;
+            }
+            case 'x':
+            {            
+                string arg_exit_code{optarg};
+                try {
+                    exit_code = stoi(arg_exit_code);
+                } catch (const std::exception& e){
+                    std::cerr << "Exception: " << e.what() << std::endl;
+                } catch (...) {
+                    std::cerr << "Undefined Error: " << typeid(std::current_exception()).name() << std::endl;
+                }
+
+                break;
+            }
+        }
     }
 
     cout << getpid() << ":" << getppid() << endl;
+
+    if (sleep_time > 0) {
+        sleep(sleep_time);
+    }
+
+    if (exit_code != 0) {
+        exit(exit_code);
+    }
 }
